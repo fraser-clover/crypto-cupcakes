11356d081261c590dfa3634e97a4e14ce574853b
Fraser Clover <leesel.fraser@clover.com>
2023-04-19 18:02:45 -0400

    add

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-require('dotenv').config('.env');
-const cors = require('cors');
-const express = require('express');
+require("dotenv").config(".env");
+const cors = require("cors");
+const express = require("express");
 const app = express();
-const morgan = require('morgan');
+const morgan = require("morgan");
 const { PORT = 3000 } = process.env;
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 // TODO - require express-openid-connect and destructure auth from it
-const { auth } = require('express-openid-connect');
+const { auth } = require("express-openid-connect");
 
 const {
-  AUTH0_SECRET , // generate one by using: `openssl rand -base64 32`
+  AUTH0_SECRET, // generate one by using: `openssl rand -base64 32`
   AUTH0_AUDIENCE,
   AUTH0_CLIENT_ID,
   AUTH0_BASE_URL,
@@ -17,7 +17,7 @@ const {
 } = process.env;
 
 const config = {
-  authRequired: true, // this is different from the documentation
+  authRequired: false, // this is different from the documentation
   auth0Logout: true,
   secret: AUTH0_SECRET,
   baseURL: AUTH0_AUDIENCE,
@@ -25,59 +25,73 @@ const config = {
   issuerBaseURL: AUTH0_BASE_URL,
 };
 
-const { User, Cupcake } = require('./db');
+const { User, Cupcake } = require("./db");
 
 // middleware
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 /* *********** YOUR CODE HERE *********** */
 // follow the module instructions: destructure config environment variables from process.env
 // follow the docs:
-  // define the config object
-  // attach Auth0 OIDC auth router
-  // create a GET / route handler that sends back Logged in or Logged out
+// define the config object
+// attach Auth0 OIDC auth router
+// create a GET / route handler that sends back Logged in or Logged out
 
-  // auth router attaches /login, /logout, and /callback routes to the baseURL
+// auth router attaches /login, /logout, and /callback routes to the baseURL
 app.use(auth(config));
 
-
 app.use(async (req, res, next) => {
-  const [user] = await User.findOrCreate({
-  where: {
-     username: req.oidc.user.nickname,
-     name: req.oidc.user.name,
-     email: req.oidc.user.email
-  }
-  });
-  
-  console.log(req);
-  console.log(user);
-  next();
-
   // stuff for bonus
   // first, change authrequired in config to false
-  //   const auth = req.header('Authorization')
-//   const token = auth.split(' ')[1];
+  const auth = req.header("Authorization");
+  const token = auth.split(" ")[1];
 
-// jwt.verify
+  const isValid = jwt.verify(token);
+  if (isValid) {
+    const data = req.user;
+    res.send(data);
+    next();
+  }
+
+  // const [user] = await User.findOrCreate({
+  // where: {
+  //    username: req.oidc.user.nickname,
+  //    name: req.oidc.user.name,
+  //    email: req.oidc.user.email
+  // }
+  // });
+
+  // console.log(req);
+  // console.log(user);
+  // next();
 });
 
+
 // req.isAuthenticated is provided from the auth router
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   console.log(req.oidc.user);
-  res.send(req.oidc.isAuthenticated() ? `Logged in 
+  res.send(
+    req.oidc.isAuthenticated()
+      ? `Logged in 
   <h1 style="color:#008900; text-align:center">My Web App, Inc.</h1>
   <h1>Welcome, ${req.oidc.user.given_name} ${req.oidc.user.family_name} </h1>
   <p><strong>Username: ${req.oidc.user.nickname}</strong></p>
   <p>${req.oidc.user.email}</p>
-  <img src="${req.oidc.user.picture}" />` : 'Logged out');
+  <img src="${req.oidc.user.picture}" />`
+      : "Logged out"
+  );
 });
 
-app.get('/cupcakes', async (req, res, next) => {
+app.get("/cupcakes", async (req, res, next) => {
   try {
+    if (!req.oidc.user) {
+      console.error("no user");
+    }
+    const { title, flavor, stars } = req.body;
+    const { userId } = req.user.id;
     const cupcakes = await Cupcake.findAll();
     res.send(cupcakes);
   } catch (error) {
@@ -86,28 +100,27 @@ app.get('/cupcakes', async (req, res, next) => {
   }
 });
 
-app.get('/me', async (req, res, next) => {
+app.get("/me", async (req, res, next) => {
   const user = await User.findOne({
     where: {
       username: req.oidc.user.nickname,
     },
     raw: true,
-  })
+  });
 
   if (user) {
-    const token = jwt.sign(user, JWT_SECRET, { expiresIn: '1w' });
-    res.send({user, token})
+    const token = jwt.sign(user, JWT_SECRET, { expiresIn: "1w" });
+    res.send({ user, token });
   }
 });
 
 // error handling middleware
 app.use((error, req, res, next) => {
-  console.error('SERVER ERROR: ', error);
-  if(res.statusCode < 400) res.status(500);
-  res.send({error: error.message, name: error.name, message: error.message});
+  console.error("SERVER ERROR: ", error);
+  if (res.statusCode < 400) res.status(500);
+  res.send({ error: error.message, name: error.name, message: error.message });
 });
 
 app.listen(PORT, () => {
   console.log(`Cupcakes are ready at http://localhost:${PORT}`);
 });
-
